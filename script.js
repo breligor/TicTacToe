757236b08f0a3ff2e33871f524b04deeb16488a8
breligor <77774848+breligor@users.noreply.github.com>
2022-06-25 08:11:59 +0300

    game field on html

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+/*
+let container = document.createElement('section');
+document.body.append(container);
+container.classList.add('container')
+let board = document.createElement('div');
+container.append(board);
+board.classList.add('board')
+for (let i = 1; i <= 9; i++) {
+    let div = document.createElement('div');
+    board.append(div);
+    div.classList.add('box')
+}
+*/
