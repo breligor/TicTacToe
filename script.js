c533c8902445b7ca8f65165bd7d38d58653f9e28
breligor <77774848+breligor@users.noreply.github.com>
2022-06-28 14:50:25 +0300

    TTT 3x3
    Крестики нолики 3x3
    Версия немасштабируемая

--- script.js
+++ script.js
@@ -1,16 +1,72 @@
-
-
-
-/*
-let container = document.createElement('section');
-document.body.append(container);
-container.classList.add('container')
-let board = document.createElement('div');
-container.append(board);
-board.classList.add('board')
-for (let i = 1; i <= 9; i++) {
-    let div = document.createElement('div');
-    board.append(div);
-    div.classList.add('box')
+let result = ''; //переменная для объявления победителя
+let step = 0; //счетчик хода
+let resultMessage = document.getElementById('resultMessage'); // html-блок объявления победителя
+const boxes = document.getElementsByClassName('block'); // ячейка
+const game = document.getElementById('game'); //все игровое поле
+// создание поля 3х3
+for (let i = 0; i < 9; i++) {
+    document.getElementById('game').innerHTML += `<div class="block"><div>`;
 }
-*/
+// игра
+    game.onclick = function (event)
+    {
+        if (event.target.className === 'block' && event.target.textContent === "") //нахождение блока и проверка от повторного нажатия
+        {
+            if  (step % 2 === 0 ) {
+                event.target.innerHTML = 'X';
+            }
+            else {
+                event.target.innerHTML = '0';
+            }
+            step++;
+            checkWinner();
+        }
+    };
+//функция проверки победителя
+function checkWinner () {
+//массив все выигрышных вариантов
+    const arr = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < arr.length; i++)
+    {
+        if (boxes[arr[i][0]].innerHTML === 'X'
+            && boxes[arr[i][1]].innerHTML === 'X'
+            && boxes[arr[i][2]].innerHTML === 'X'
+        )
+        {
+            result = 'крестики победили';
+            doResult(result);
+        }
+        else
+            if (boxes[arr[i][0]].innerHTML === '0'
+                && boxes[arr[i][1]].innerHTML === '0'
+                && boxes[arr[i][2]].innerHTML === '0'
+            )
+            {
+                result = 'нолики победили';
+                doResult(result);
+
+            }
+//проверка на ничью
+            if (step === 9 && boxes[arr[i][0]].innerHTML !== ''
+            && boxes[arr[i][1]].innerHTML !== ''
+            && boxes[arr[i][2]].innerHTML !== '')
+            {
+                result = 'ничья';
+                doResult(result);
+
+            }
+    }
+}
+//объявление победителя
+let doResult = winner => {
+resultMessage.innerText = winner;
+}
